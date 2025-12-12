e7423826f01119cb2ad738f099e5434d0859589d
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-12-12 19:37:45 -0300

    Add files via upload

---
+++ fundamentos/print.py
@@ -0,0 +1,7 @@
+print(12, 34, sep='-', end='\r\n')
+print(12, 34, sep='-', end='##')
+print(12, 34, sep='-', end='##\n')
+print(12, 34, sep="-")
+
+#A função type mostra o tipo que o Python inferiu ao valor
+print(type('Pedro'))
