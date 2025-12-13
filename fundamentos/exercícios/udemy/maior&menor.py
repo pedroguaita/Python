6139c6558541fe87765f8c94b974e19c152db747
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-12-13 16:28:15 -0300

    Add files via upload

---
+++ fundamentos/exercícios/udemy/maior&menor.py
@@ -0,0 +1,11 @@
+p1 = input('Digite um valor: ')
+p2 = input('Digite outro valor: ')
+
+if p1 > p2:
+    print(f'O número {p1=} é maior que {p2=}.')
+elif p1 < p2:
+    print(f'O número {p2=} é maior que {p1=}.')
+else:
+    print(f'Os números são iguais.')
+
+    
